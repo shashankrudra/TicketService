1b35410a2b7286333a59beee1b318a2d8cbf2ac7
Shashank Singh <shashank.rudra@gmail.com>
2019-02-08 00:52:37 -0500

    created project template

---
+++ src/main/java/com/walmart/congo/service/TicketServiceImpl.java
@@ -0,0 +1,22 @@
+package com.walmart.congo.service;
+
+import com.walmart.congo.model.SeatHold;
+
+public class TicketServiceImpl implements TicketService {
+
+	@Override
+	public int numSeatsAvailable() {
+		return 0;
+	}
+
+	@Override
+	public SeatHold findAndHoldSeats(int numSeats, String customerEmail) {
+		return null;
+	}
+
+	@Override
+	public String reserveSeats(int seatHoldId, String customerEmail) {
+		return null;
+	}
+
+}
